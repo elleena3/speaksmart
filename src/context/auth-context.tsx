d68dcf77d1f892a94f869811222986f50699948c
김민철 <elleena3@gmail.com>
2025-07-25 04:52:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -202,5 +202,4 @@ export const useAuth = (): AuthContextType => {
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  return context;
-};
+  
